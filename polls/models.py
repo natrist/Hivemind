1579707984c0cfe4c7b99e9713662f086fe625e6
Tristan Cormier <tristanc@openmailbox.org>
2018-03-07 20:31:57 -0500

    Linked the User model with the UserProfile one, allowing save/edit sync.

--- polls/models.py
+++ polls/models.py
@@ -3,11 +3,23 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# UserProfile table model
 class UserProfile(models.Model):
-    user   = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField()
-    # avatar = models.ImageField(upload_to = 'media/', default = 'media/e/no-img.jpg')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
 
 # Category table model
 class Category(models.Model):
